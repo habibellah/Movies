4f87fd22bfb57569b595e13adb50cc6240776c50
habibellah <habibayata1@gmail.com>
2023-01-27 14:55:17 +0100

    Add Navigation Bottom

--- app/src/main/java/habibellah/ayata/movies/MainActivity.kt
+++ app/src/main/java/habibellah/ayata/movies/MainActivity.kt
@@ -5,11 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import habibellah.ayata.movies.ui.BottomNavItem
+import habibellah.ayata.movies.ui.BottomNavigationBar
+import habibellah.ayata.movies.ui.Navigation
 import habibellah.ayata.movies.ui.theme.MoviesTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,12 +27,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MoviesTheme {
-
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = {
+                        BottomNavigationBar(items = listOf(
+                            BottomNavItem(
+                                name = "Home",
+                                route = "home",
+                                icon = Icons.Default.Home,
+                                badgeCount = 520
+                                ),
+                            BottomNavItem(
+                                name = "Search",
+                                route = "search",
+                                icon = Icons.Default.Search,
+                                badgeCount = 52
+                            ),
+                            BottomNavItem(
+                                name = "Favorite",
+                                route = "favorite",
+                                icon = Icons.Default.Favorite
+                            ),
+                            BottomNavItem(
+                                name = "Profile",
+                                route = "profile",
+                                icon = Icons.Default.Person
+                            ),
+                        ), navController = navController,
+                            onItemClick = {
+                                navController.navigate(it.route)
+                            })
+                    }
                 ) {
-                    Greeting("Android")
+                    Navigation(navController = navController)
                 }
             }
         }
