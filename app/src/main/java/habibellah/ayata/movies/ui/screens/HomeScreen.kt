ef901b581e2ec3a12cc7b7b5a2a596accc2bbb07
habibellah <habibayata1@gmail.com>
2023-01-30 21:19:44 +0100

    Home Screen Starting UI

--- app/src/main/java/habibellah/ayata/movies/ui/screens/HomeScreen.kt
+++ app/src/main/java/habibellah/ayata/movies/ui/screens/HomeScreen.kt
@@ -1,10 +1,52 @@
 package habibellah.ayata.movies.ui.screens
 
 
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import habibellah.ayata.movies.ui.composables.MovieItemWithCategoryName
 
 @Composable
-fun HomeScreen(){
-    MovieItemWithCategoryName(movieCategory = "Top Rated")
+fun HomeScreen() {
+    LazyColumn {
+
+    }
 }
+    @Composable
+    fun PopularMovieLazyRow(){
+
+    }
+
+    @Composable
+    fun TvShowsLazyGrid(){
+
+    }
+
+    @Composable
+    fun OnTheAirLazyRow(){
+
+    }
+
+    @Composable
+    fun TrendingLazyRow(){
+
+    }
+
+    @Composable
+    fun AiringTodayLazyGrid(){
+
+    }
+
+    @Composable
+    fun NowStreamingLazyRow(){
+
+    }
+
+    @Composable
+    fun UpComingLazyRow(){
+
+    }
+
+    fun MySteryLazyRow(){
+
+    }
+
